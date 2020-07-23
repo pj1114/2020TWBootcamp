cd868f60ee01606cc767d044881a67ad9d22589b
pj1114 <b03701204@ntu.edu.tw>
2020-07-23 16:34:41 +0800

    First version of spelling error corrector

---
+++ ZaiLaGan/setup.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+
+# Install required python packages
+pip install transformers --upgrade
+pip install torch --upgrade
+pip install zhon --upgrade
+pip install Flask --upgrade
+pip install line-bot-sdk --upgrade
+pip install pyyaml --upgrade
+pip install typing --upgrade
+pip install numpy --upgrade
+pip install pandas --upgrade
+pip install pickle --upgrade
---
+++ ZaiLaGan/utilities/trie.py
@@ -0,0 +1,41 @@
+from typing import *
+
+class TrieNode():
+	def __init__(self, token: str):
+		self.token = token
+		self.children = {}
+		self.isWord = False
+		self.frequency = -1
+
+	# Add a child node (connect to next token)
+	def addChild(self, child):
+		self.children[child.token] = child
+
+	# Record that the node is end of a word
+	def setIsWord(self):
+		self.isWord = True
+
+	# Record word frequency
+	def setWordFrequency(self, frequency: int):
+		self.frequency = frequency
+
+class Trie():
+	def __init__(self):
+		self.root = TrieNode(None)
+
+	# Add a new word into trie
+	def addWord(self, word: str, frequency: int):
+		currentNode = self.root
+		for i in range(len(word)):
+			token = word[i]
+			# Check if the token has already existed in the trie
+			if(token in currentNode.children):
+				currentNode = currentNode.children[token]
+			else:
+				nextNode = TrieNode(token)
+				currentNode.children[token] = nextNode
+				currentNode = nextNode
+			# Check if this is the last token
+			if(i == len(word)-1):
+				currentNode.setIsWord()
+				currentNode.setWordFrequency(frequency)
