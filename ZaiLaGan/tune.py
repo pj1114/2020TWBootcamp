df1afd647ba2126220dc7ab784f5382db385b4b0
pj1114 <b03701204@ntu.edu.tw>
2020-08-01 08:27:08 +0800

    Edit tune.py for tuning detection threshold

--- ZaiLaGan/tune.py
+++ ZaiLaGan/tune.py
@@ -11,7 +11,7 @@ with open("./config.yml", "r") as config_file_yaml:
 
 # Concatenate root path with all subpaths
 root_path = config["User"]["user_path"]
-model_names = ["gpt2_chinese", "ner"]
+model_names = ["gpt2_chinese", "ner", "ngram"]
 data_names = ["pinyin", "stroke", "dictionary", "common_char_set", "confusion", "place", "person", "ssc"]
 for model_name in model_names:
 	config["Model"][model_name] = os.path.join(root_path, config["Model"][model_name])
@@ -29,35 +29,47 @@ path = "./data/spelling_error/sighan_2013.pkl"
 file = open(path, "rb")
 pairs = pickle.load(file)
 
-# Calculate metrics
-y_true = []
-y_pred = []
-for pair in pairs:
+# Calculate metrics for different detection thresholds
+thresholds = [1e-5, 3e-5, 5e-5, 1e-4, 3e-4, 5e-4, 1e-3, 3e-3, 5e-3, 1e-2]
+results = {}
+for threshold in thresholds:
+	y_true = []
+	y_pred = []
+	# Iterate through each sentence to perform spelling error detection
+	for pair in pairs:
+		try:
+			wrong, err_positions_true = pair[0], pair[1]
+			# Perform named-entity recognition first
+			ner_processed_text, ne_positions = ZLG.detectNamedEntity([wrong])[0]
+			ne_positions = set(ne_positions)
+			# Detect spelling errors
+			err_positions, bert_predictions = ZLG.detectSpellingError(ner_processed_text, threshold, 5)
+			# Remove potential spelling errors included in any named-entity
+			err_positions_pred = []
+			for err_position in err_positions:
+				if(err_position not in ne_positions):
+					err_positions_pred.append(err_position)
+			y_true.append(err_positions_true)
+			y_pred.append(err_positions_pred)
+		except:
+			traceback.print_exc()
 	try:
-		wrong, err_positions_true = pair[0], pair[1]
-		# Perform named-entity recognition first
-		ner_processed_text, ne_positions = ZLG.detectNamedEntity([wrong])[0]
-		ne_positions = set(ne_positions)
-		# Detect spelling errors
-		err_positions, bert_predictions = ZLG.detectSpellingError(ner_processed_text, 5e-4, 5)
-		# Remove potential spelling errors included in any named-entity
-		err_positions_pred = []
-		for err_position in err_positions:
-			if(err_position not in ne_positions):
-				err_positions_pred.append(err_position)
-		print(err_positions_pred, err_positions_true)
-		y_true.append(err_positions_true)
-		y_pred.append(err_positions_pred)
+		# Store the results
+		result = {}
+		result["false_alarm_rate"] = SEM.false_alarm_rate(y_pred, y_true)
+		result["detection_accuracy"] = SEM.detection_accuracy(y_pred, y_true)
+		result["detection_recall"] = SEM.detection_recall(y_pred, y_true)
+		result["detection_precision"] = SEM.detection_precision(y_pred, y_true)
+		result["detection_f1"] = SEM.detection_f1(y_pred, y_true)
+		result["error_location_accuracy"] = SEM.error_location_accuracy(y_pred, y_true)
+		result["error_location_precision"] = SEM.error_location_precision(y_pred, y_true)
+		result["error_location_recall"] = SEM.error_location_recall(y_pred, y_true)
+		result["error_location_f1"] = SEM.error_location_f1(y_pred, y_true)
+		results[threshold] = result
+		print(threshold, result)
 	except:
 		traceback.print_exc()
 
-# Show the results
-print("False Alarm Rate: ", SEM.false_alarm_rate(y_pred, y_true))
-print("Detection Accuracy: ", SEM.detection_accuracy(y_pred, y_true))
-print("Detection Recall: ", SEM.detection_recall(y_pred, y_true))
-print("Detection Precision: ", SEM.detection_precision(y_pred, y_true))
-print("Detection F1: ", SEM.detection_f1(y_pred, y_true))
-print("Error Location Accuracy: ", SEM.error_location_accuracy(y_pred, y_true))
-print("Error Location Precision: ", SEM.error_location_precision(y_pred, y_true))
-print("Error Location Recall: ", SEM.error_location_recall(y_pred, y_true))
-print("Error Location F1: ", SEM.error_location_f1(y_pred, y_true))
+# Store the results in a pickle file
+with open("./tune_result.pkl", "wb") as tune_result:
+	pickle.dump(results, tune_result)
